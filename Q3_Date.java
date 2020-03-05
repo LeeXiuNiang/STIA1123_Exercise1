fc4428fc74429ba3f3df2a129ffde36fcbc78712
Lee Xiu Niang <55247305+LeeXiuNiang@users.noreply.github.com>
2020-03-05 16:39:35 +0800

    Create Q3_Date.java

---
+++ Q3_Date.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
+
+public class Main {
+
+    public static void main(String[] args) throws ParseException {
+        //Q3
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter date in format of dd/MM/yy:");
+        String strdate=input.next();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yy");
+        Date date = formatter.parse(strdate);
+        System.out.print("Date format with dd.MM.yy: ");
+        System.out.println(new SimpleDateFormat("dd.MM.yy").format(date));
+    }
+}
