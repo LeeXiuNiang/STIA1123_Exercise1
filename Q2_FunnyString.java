5e57d53c52b5407a2e9cd8abc23a716ae1a0462e
Lee Xiu Niang <55247305+LeeXiuNiang@users.noreply.github.com>
2020-03-05 16:31:08 +0800

    Create Q2_FunnyString.java

---
+++ Q2_FunnyString.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FunnyString {
+
+
+    public static void main(String[] args) {
+
+        //Q2
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter a colour: ");
+        String colour = input.nextLine();
+        System.out.print("Enter a food: ");
+        String food = input.nextLine();
+        System.out.print("Enter a animal: ");
+        String animal = input.nextLine();
+        System.out.print("Enter a friend: ");
+        String friend = input.nextLine();
+        System.out.println("I had a dream that " + friend + " ate a "+ colour + " "+ animal+" and said it tasted like "+ food +"!");
+
+    }
+}
